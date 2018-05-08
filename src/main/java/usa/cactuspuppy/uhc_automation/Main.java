a3e9ab0a014e57d24d16f264471482ed901ff44f
CactusPuppy <coby.zhong@gmail.com>
2018-05-08 02:54:18 -0700

    Rewrote creation of config, added additional info to startup

--- src/main/java/usa/cactuspuppy/uhc_automation/Main.java
+++ src/main/java/usa/cactuspuppy/uhc_automation/Main.java
@@ -4,10 +4,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
 
 public class Main extends JavaPlugin {
     protected GameInstance gi;
@@ -18,16 +20,15 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        saveDefaultConfig();
+        long start = System.currentTimeMillis();
+        createConfig();
         BukkitRunnable r = new BukkitRunnable() {
             @Override
             public void run() {
                 try {
                     initSQL();
                     statement = connection.createStatement();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                } catch (ClassNotFoundException e) {
+                } catch (SQLException | ClassNotFoundException e) {
                     e.printStackTrace();
                 }
             }
@@ -37,11 +38,36 @@ public class Main extends JavaPlugin {
         this.getCommand("uhcstart").setExecutor(new CommandStart(this));
         this.getCommand("uhcoptions").setExecutor(new CommandOptions(this));
         Bukkit.getServer().getPluginManager().registerEvents(new WorldChangeListener(), this);
+
+        getLogger().info("UHC Automation loaded in " + ((double) (System.currentTimeMillis() - start)) + " ms");
     }
 
     @Override
     public void onDisable() {
         saveDefaultConfig();
+    }
+
+    /**
+     * @source Innectic's Permissify plugin. Blatantly stolen code btw.
+     */
+    private void createConfig() {
+        try {
+            if (!getDataFolder().exists()) {
+                boolean created = getDataFolder().mkdirs();
+                if (!created) {
+                    getLogger().log(Level.SEVERE, "Could not create config folder!");
+                }
+                File config = new File(getDataFolder(), "config.yml");
+                if (!config.exists()) {
+                    getLogger().info("config.yml not found, creating...");
+                    saveDefaultConfig();
+                } else {
+                    getLogger().info("Loading config.yml...");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void initSQL() throws SQLException, ClassNotFoundException {
