8cd699fe3643f247136a197606530c89554a9f46
CactusPuppy <coby.zhong@gmail.com>
2018-05-08 03:17:12 -0700

    Added FinalBorderSize check and terminated execution earlier upon failed inputs

--- src/main/java/usa/cactuspuppy/uhc_automation/CommandOptions.java
+++ src/main/java/usa/cactuspuppy/uhc_automation/CommandOptions.java
@@ -25,6 +25,13 @@ public class CommandOptions implements CommandExecutor {
                 if (args[0].equals(OPTIONS[0])) {
                     main.gi.setInitSize(Integer.valueOf(args[1]));
                 } else if (args[0].equals(OPTIONS[1])) {
+                    if (Integer.valueOf(args[1]) > main.gi.getFinalSize()) {
+                        commandSender.sendMessage(
+                                ChatColor.RED + "ERROR: Requested final border size " + ChatColor.RESET + Integer.valueOf(args[1])
+                                        + ChatColor.RED + " is not smaller than current initial border size "
+                                        + ChatColor.RESET + main.gi.getInitSize());
+                        return true;
+                    }
                     main.gi.setFinalSize(Integer.valueOf(args[1]));
                 } else if (args[0].equals(OPTIONS[2])) {
                     main.gi.setTimeToShrink(Integer.valueOf(args[1]));
@@ -35,6 +42,7 @@ public class CommandOptions implements CommandExecutor {
                         main.gi.setTeamMode(false);
                     } else {
                         commandSender.sendMessage(ChatColor.RED + "ERROR: Option " + args[1] + " is not true or false.");
+                        return true;
                     }
                 }
             } catch (NumberFormatException e) {
