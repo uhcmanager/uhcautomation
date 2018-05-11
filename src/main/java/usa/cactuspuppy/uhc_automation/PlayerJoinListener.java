1d4705991784c7c320b650590b930d1bf0088349
CactusPuppy <coby.zhong@gmail.com>
2018-05-11 16:22:44 -0700

    Added PlayerJoin listener

---
+++ src/main/java/usa/cactuspuppy/uhc_automation/PlayerJoinListener.java
@@ -0,0 +1,12 @@
+package usa.cactuspuppy.uhc_automation;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerJoinListener implements Listener {
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        //TODO: Mark players as joined
+    }
+}
