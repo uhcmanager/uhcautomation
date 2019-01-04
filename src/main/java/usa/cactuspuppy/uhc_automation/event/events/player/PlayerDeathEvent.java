0c3bff14130508cbb1771825e872d7873b319946
CactusPuppy <coby.zhong@gmail.com>
2019-01-03 20:01:58 -0800

    Update .gitignore

--- src/main/java/usa/cactuspuppy/uhc_automation/event/events/player/PlayerDeathEvent.java
+++ src/main/java/usa/cactuspuppy/uhc_automation/event/events/player/PlayerDeathEvent.java
@@ -3,11 +3,13 @@ package usa.cactuspuppy.uhc_automation.event.events.player;
 import lombok.Getter;
 import org.bukkit.entity.Player;
 import usa.cactuspuppy.uhc_automation.GameInstance;
-import usa.cactuspuppy.uhc_automation.event.events.player.PlayerEvent;
 
 import java.util.UUID;
 
 public class PlayerDeathEvent extends PlayerEvent {
+    /**
+     * Whether or not this death removes the player from the game
+     */
     @Getter private boolean isFinal;
 
     public PlayerDeathEvent(GameInstance gameInstance, UUID u) {
