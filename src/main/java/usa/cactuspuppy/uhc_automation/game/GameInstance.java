f4c14c7f17c8e5970804b236a696fff422ce6057
CactusPuppy <coby.zhong@gmail.com>
2019-01-20 22:45:19 -0800

    update config, continue game instance setup

--- src/main/java/usa/cactuspuppy/uhc_automation/game/GameInstance.java
+++ src/main/java/usa/cactuspuppy/uhc_automation/game/GameInstance.java
@@ -1,18 +1,48 @@
 package usa.cactuspuppy.uhc_automation.game;
 
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.Set;
+import java.util.UUID;
 
+@Getter
 public abstract class GameInstance implements Serializable {
+
+    // [=== MAIN INFO ===]
+
     /**
-     * Unique name by which a game is identified.<br>Must match regex (-_A-Za-z0-9){1-255}
+     * Unique identifier for this game instance. This should be set once upon game creation and never changed until the game ends and/or is deleted.
      */
-    @Getter @Setter(AccessLevel.PACKAGE) private String name;
+    private long gameID;
+
     /**
-     * Display-friendly name for this game. May contain spaces and any other characters.
+     * Marker of when the game begins (i.e. when players are released) as given by {@code System.currentTimeMillis()}
      */
-    @Getter @Setter private String displayName;
+    private long startTime;
+
+    /**
+     * Name for this game. May or may not be unique; should not be used to identify this game.
+     */
+    private String name;
+
+    /**
+     * Unique ID of main game world as given by {@code World::getUID}.
+     */
+    private UUID mainWorld;
+
+    /**
+     * Other worlds that players may travel to during the course of the game without being considered as leaving the game. Examples include the main world's Nether and End dimension.
+     */
+    private Set<UUID> otherWorlds;
+
+    // [=== PLAYER INFO ===]
+
+    private Set<UUID> alivePlayers;
+
+    private int initNumPlayers;
+
+    private Set<UUID> spectators;
+
+    private long epLength;
 }
