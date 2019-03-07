b61db9fecaa0e1718fa396928cd9edf8fe0d5371
CactusPuppy <coby.zhong@gmail.com>
2019-03-06 23:23:05 -0800

    Make gource overwrite output file

--- gource.sh
+++ gource.sh
@@ -1,1 +1,2 @@
-gource -s 1 -a 1 -1920x1080 -o - -r 30 --user-image-dir "E:/Documents/gource-users" | ffmpeg -i pipe:0 -f mp4 "E:/Videos/RawMaterial/gource.mp4"
+#@IgnoreInspection BashAddShebang
+gource -s 1 -a 1 -1920x1080 -o - -r 30 --user-image-dir "E:/Documents/gource-users" | ffmpeg -y -i pipe:0 -f mp4 "E:/Videos/RawMaterial/gource.mp4"
