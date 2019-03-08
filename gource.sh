556eaf1bd51540c91994c5eede748f88d35541c8
CactusPuppy <coby.zhong@gmail.com>
2019-03-08 00:18:26 -0800

    Updated gource.sh

--- gource.sh
+++ gource.sh
@@ -1,2 +1,3 @@
 #@IgnoreInspection BashAddShebang
 gource -s 1 -a 1 -1920x1080 -o - -r 30 --user-image-dir "E:/Documents/gource-users" | ffmpeg -y -i pipe:0 -f mp4 "E:/Videos/RawMaterial/gource.mp4"
+read -n1 -r -p "Press any key to continue..." key
