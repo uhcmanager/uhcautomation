7a54242d3fd8b5a94c173e9ecb19a66a1e94dfc8
CactusPuppy <coby.zhong@gmail.com>
2019-03-19 17:16:45 -0700

    Added basic broadcast functionality to UHCUtils

--- src/main/java/usa/cactuspuppy/uhc_automation/utils/UHCUtils.java
+++ src/main/java/usa/cactuspuppy/uhc_automation/utils/UHCUtils.java
@@ -1,13 +1,35 @@
 package usa.cactuspuppy.uhc_automation.utils;
 
+import lombok.AllArgsConstructor;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import usa.cactuspuppy.uhc_automation.game.GameInstance;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 
+@AllArgsConstructor
 public final class UHCUtils {
+    private GameInstance instance;
 
     //TODO: Broadcast methods
+    public void broadcastChatMessage(String chatMessage) {
+        instance.getAllPlayers().stream().map(Bukkit::getPlayer).filter(Objects::nonNull).forEach(
+                p -> p.sendMessage(chatMessage)
+        );
+    }
 
+    public void broadcastSound(Sound sound, float pitch) {
+        instance.getAllPlayers().stream().map(Bukkit::getPlayer).filter(Objects::nonNull).forEach(
+                p -> p.playSound(p.getLocation(), sound, 10.0F, pitch)
+        );
+    }
+
+    public void broadcastTitle(String title, String subtitle, int in, int stay, int out) {
+        instance.getAllPlayers().stream().map(Bukkit::getPlayer).filter(Objects::nonNull).forEach(
+                p -> p.sendTitle(title, subtitle, in, stay, out)
+        );
+    }
 }
