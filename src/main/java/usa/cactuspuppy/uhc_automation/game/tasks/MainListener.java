fa0537784e4b1bc1f0f385557e5d07657f6052c7
CactusPuppy <coby.zhong@gmail.com>
2019-05-23 21:28:33 +0800

    Mark successful UHC start

--- src/main/java/usa/cactuspuppy/uhc_automation/game/tasks/MainListener.java
+++ src/main/java/usa/cactuspuppy/uhc_automation/game/tasks/MainListener.java
@@ -3,4 +3,5 @@ package usa.cactuspuppy.uhc_automation.game.tasks;
 import org.bukkit.event.Listener;
 
 public class MainListener implements Listener {
+    //TODO: Implement join listening and world switch listening
 }
